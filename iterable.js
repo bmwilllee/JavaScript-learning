0be1c659c1fa70e882f415de348af2bdc34d21c3
willlee <986852442@qq.com>
2017-07-03 00:53:35 +0800

    some iterablr methods for array, set and map

---
+++ iterable.js
@@ -0,0 +1,80 @@
+//					iterable
+
+//	遍历Array可以采用下标循环，遍历Map和Set就无法使用下标
+//	为了统一集合类型，ES6标准引入了新的iterable类型，Array、Map和Set都属于iterable类型
+
+//	具有iterable类型的集合可以通过新的for ... of循环来遍历
+
+//	用for ... of循环遍历集合，用法如下：
+
+var a = [1, 2, 3, 4, 5];
+var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
+var s = new Set(['A', 'B', 'C']);
+
+for (var i of a){
+	alert(i);
+}
+
+for (var i of m){
+	alert(i[0] + '=' + i[1]);	// 注意 Map 的 key 和 value 的获取方式
+}
+
+for (var i of s){
+	alert(i);
+}
+
+//	你可能会有疑问，for ... of循环和for ... in循环有何区别？
+//	for ... in循环由于历史遗留问题，它遍历的实际上是对象的属性名称
+//	一个Array数组实际上也是一个对象，它的每个元素的索引被视为一个属性
+
+//	当我们手动给Array对象添加了额外的属性后，for ... in循环将带来意想不到的意外效果：
+
+var a = ['A', 'B', 'C'];
+a.name = 'Hello';
+for (var x in a) {
+    alert(x); // '0', '1', '2', 'name'
+}
+
+//	for ... in循环将把name包括在内，但Array的length属性却不包括在内
+
+//	for ... of循环则完全修复了这些问题，它只循环集合本身的元素：
+var a = ['A', 'B', 'C'];
+a.name = 'Hello';
+for (var x of a) {
+    alert(x); // 'A', 'B', 'C'
+}
+
+//	这就是为什么要引入新的for ... of循环
+//	然而，更好的方式是直接使用iterable内置的forEach方法
+//	它接收一个函数，每次迭代就自动回调该函数
+//	以Array为例：
+var s = ['A', 'B', 'C'];
+
+s.forEach(function(element, index, array){
+	// element: 指向当前元素的值
+    // index: 指向当前索引
+    // array: 指向Array对象本身
+	alert(element);
+})
+
+//	Set与Array类似，但Set没有索引，因此回调函数的前两个参数都是元素本身：
+var s = new Set(['A', 'B', 'C']);
+
+s.forEach(function (element, sameElement, set) {
+    alert(element);
+});
+
+//	Map的回调函数参数依次为value、key和map本身：
+var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
+
+m.forEach(function (value, key, map) {
+    alert(value);
+});
+
+//	如果对某些参数不感兴趣，由于JavaScript的函数调用不要求参数必须一致
+//	因此可以忽略它们。例如，只需要获得Array的element：
+var a = ['A', 'B', 'C'];
+
+a.forEach(function (element) {
+    alert(element);
+});
